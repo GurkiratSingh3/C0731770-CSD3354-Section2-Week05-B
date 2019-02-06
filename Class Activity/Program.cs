aac9d86f474b3b65626ac12de2e812242cd4b9d1
Gurkirat Singh <c0731779@mylambton.ca>
2019-02-06 11:37:07 -0500

    11.30AM

--- Class Activity/Program.cs
+++ Class Activity/Program.cs
@@ -10,6 +10,60 @@ namespace Class_Activity
     {
         static void Main(string[] args)
         {
+            LinkedList a = new LinkedList();
+            a.run();
+            a.walkOverTheList();
+        }
+    }
+    class Dog
+    {
+        public static int NumberofDogs = 0;
+        public Dog()
+        {
+            Dog.NumberofDogs++;
+        }
+        public String DogName;
+        public String DogBreed;
+        public Dog nextDog;
+
+    }
+    class LinkedList
+    {
+        public Dog Head;
+        public Dog current;
+        public Dog Peanut = new Dog();
+        public Dog Fifi = new Dog();
+        public Dog Jordan = new Dog();
+        public Dog Fido = new Dog();
+
+        public void run()
+        {
+            Head = Peanut;
+            Peanut.DogName = "Peanut";
+            Peanut.DogBreed = "Bichon";
+            Peanut.nextDog = Fifi;
+
+            Fifi.DogName = "Fifi";
+            Fifi.DogBreed = "Poodle";
+            Fifi.nextDog = Jordan;
+
+            ;
+            Jordan.DogName = "Jordan";
+            Jordan.DogBreed = "Germansheppard";
+            Jordan.nextDog = Fido;
+
+            Fido.DogName = "Fido";
+            Fido.DogBreed = "Beagle";
+            Fido.nextDog = null;
+        }
+        public void walkOverTheList()
+        {
+            current = Head;
+            while (current != null)
+            {
+                Console.WriteLine(current.DogName);
+                current = current.nextDog;
+            }
         }
     }
 }
